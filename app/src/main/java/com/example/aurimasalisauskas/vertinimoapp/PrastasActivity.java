f768c9fcbf36afa9d8375a10ee93ae50bd9e96f1
Loonixas <loonixas@gmail.com>
2018-11-24 15:51:15 +0200

    Vertinimo appsas baiamajam

---
+++ app/src/main/java/com/example/aurimasalisauskas/vertinimoapp/PrastasActivity.java
@@ -0,0 +1,58 @@
+package com.example.aurimasalisauskas.vertinimoapp;
+
+import android.app.PendingIntent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class PrastasActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.prastas_activity);
+
+        final CheckBox c1 = findViewById(R.id.checkBox);
+        final CheckBox c2 = findViewById(R.id.checkBox2);
+        final CheckBox c3 = findViewById(R.id.checkBox3);
+        final CheckBox c4 = findViewById(R.id.checkBox4);
+        final CheckBox c5 = findViewById(R.id.checkBox5);
+        Button button = findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle b = getIntent().getExtras();
+                int pasirinkimas = b.getInt("pasirinkimas");
+
+                Ivertinimas ivertinimas = new Ivertinimas();
+                ivertinimas.setPasirinkimas(pasirinkimas);
+                if (c1.isChecked()) {
+                    ivertinimas.getTrukumai().add("Personalas");
+                }
+                if (c2.isChecked()) {
+                    ivertinimas.getTrukumai().add("Inventorius");
+                }
+                if (c3.isChecked()) {
+                    ivertinimas.getTrukumai().add("Pramogos");
+                }
+                if (c4.isChecked()) {
+                    ivertinimas.getTrukumai().add("Švara");
+                }
+                if (c5.isChecked()) {
+                    ivertinimas.getTrukumai().add("Kita");
+                }
+                Database.add(ivertinimas);
+                finish();
+
+            }
+        });
+
+
+    }
+}
